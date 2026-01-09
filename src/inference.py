71b165d402f094edd004c00f66636a221ac4ae0a
Kanadesisido <takasuyari@gmail.com>
2026-01-09 11:05:48 +0900

    chore: update model checkpoint and inference configuration

--- src/inference.py
+++ src/inference.py
@@ -1,11 +1,18 @@
+import time
 from recbole.quick_start import load_data_and_model
 from recbole.data.interaction import Interaction
 import argparse
 import torch
 
-CHECKPOINT_FILE = 'saved/GRU4Rec-Dec-13-2025_23-13-07.pth'
+CHECKPOINT_FILE = 'saved/GRU4Rec-Dec-21-2025_13-56-39.pth'
 ITEM_FILE = 'ml-100k.item'
 CONFIG_FILE_LIST = ["configs/config.yaml", "configs/base.yaml"]
+
+config_dict = {
+    'use_gpu': False,
+    'device': 'cpu'
+}
+
 
 def inference_from_checkpoint():
     
@@ -13,7 +20,8 @@ def inference_from_checkpoint():
 
     print(f"設定をロード: model={config['model']}, dataset={config['dataset']}")
 
-    item_sequence = ['1', '50', '100']  # 例としてのアイテムIDシーケンス
+    start_time = time.time()
+    item_sequence = ['B09ZQN6DVM', 'B007SWAHWY', 'B0CDLRTR5L']  # 例としてのアイテムIDシーケンス
     item_length = len(item_sequence)
     pad_length = 50
 
@@ -35,8 +43,11 @@ def inference_from_checkpoint():
         "item_length": torch.tensor([item_length]),
     })
 
-    input_interaction = input_interaction.to(model.device)
+    input_interaction = input_interaction.to("cpu")
+
     scores = model.full_sort_predict(input_interaction)
+    end_time = time.time()
+    print(f"Inference Time: {end_time - start_time:.4f} seconds")
     # 最もスコアが「低い」ものを推薦する
     top_k = torch.topk(scores, k=10, largest=False)
 
